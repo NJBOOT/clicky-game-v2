b10dd167c8b9b6e25b7a19014c128ae7d8d2cf34
Nico Barbarese <nicobarbarese@gmail.com>
2020-08-13 22:54:44 -0400

    Updates

---
+++ precache-manifest.d316af5eafbfbd6e6a0257e772fe51b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4301b74298e3ec880c41ebc3cdea953",
+    "url": "/clicky-game-v2/index.html"
+  },
+  {
+    "revision": "c1b97248fe7dc85e0211",
+    "url": "/clicky-game-v2/static/css/main.c2af706f.chunk.css"
+  },
+  {
+    "revision": "4f0116eb626b9272de06",
+    "url": "/clicky-game-v2/static/js/2.842f85c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game-v2/static/js/2.842f85c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b97248fe7dc85e0211",
+    "url": "/clicky-game-v2/static/js/main.406f0b2e.chunk.js"
+  },
+  {
+    "revision": "40b5590e0be80831a07e",
+    "url": "/clicky-game-v2/static/js/runtime-main.05263ae2.js"
+  }
+]);
